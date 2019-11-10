fd0eeb7b15818bcaf655e93fdcb1b83cc19b0a25
kjendrzyca <kjendrzyca@gmail.com>
2019-11-10 13:37:01 +0100

    Use ArticleMeta in ArticlePreview

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import agent from '../agent';
 import { connect } from 'react-redux';
+
+import agent from '../agent';
 import { ARTICLE_FAVORITED, ARTICLE_UNFAVORITED } from '../constants/actionTypes';
+
+import ArticleMeta from './ArticleMeta';
 
 const FAVORITED_CLASS = 'btn btn-sm btn-primary';
 const NOT_FAVORITED_CLASS = 'btn btn-sm btn-outline-primary';
@@ -35,26 +38,16 @@ const ArticlePreview = props => {
 
   return (
     <div className="article-preview">
-      <div className="article-meta">
-        <Link to={`/@${article.author.username}`}>
-          <img src={article.author.image} alt={article.author.username} />
-        </Link>
-
-        <div className="info">
-          <Link className="author" to={`/@${article.author.username}`}>
-            {article.author.username}
-          </Link>
-          <span className="date">
-            {new Date(article.createdAt).toDateString()}
-          </span>
-        </div>
-
-        <div className="pull-xs-right">
-          <button className={favoriteButtonClass} onClick={handleClick}>
-            <i className="ion-heart"></i> {article.favoritesCount}
-          </button>
-        </div>
-      </div>
+      <ArticleMeta
+        article={article}
+        articleActions={(
+          <div className="pull-xs-right">
+            <button className={favoriteButtonClass} onClick={handleClick}>
+              <i className="ion-heart"></i> {article.favoritesCount}
+            </button>
+          </div>
+        )}
+      />
 
       <Link to={`/article/${article.slug}`} className="preview-link">
         <h1>{article.title}</h1>
