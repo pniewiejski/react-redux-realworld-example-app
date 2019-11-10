eb90fceee955c9280686b843b0c86999bbde3460
kjendrzyca <kjendrzyca@gmail.com>
2019-11-10 17:26:30 +0100

    Extract routing module

--- src/Article/ArticleActions.js
+++ src/Article/ArticleActions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link } from '../routing';
 
 import { ROOT_REDIRECT } from '../constants/actionTypes';
 import api from './api';
