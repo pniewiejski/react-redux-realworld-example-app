f64ac38346f5098b7b8b65992dd30c551ec14757
kjendrzyca <kjendrzyca@gmail.com>
2019-11-10 17:46:51 +0100

    Refactor Article to hooks

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,93 +1,91 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ArticleMeta from '../components/ArticleMeta';
 import ArticleBody from './ArticleBody';
 import CommentContainer from './CommentContainer';
 import ArticleActions from './ArticleActions';
 
-class Article extends React.Component {
-  componentWillMount() {
-    const articleId = this.props.match.params.id;
-    this.props.fetchArticle(articleId);
+function Article(props) {
+  const {
+    fetchArticle,
+    match: { params: { id: articleId }},
+    onUnload,
+  } = props;
+
+  useEffect(() => fetchArticle(articleId), [articleId, fetchArticle]);
+  useEffect(() => () => onUnload(), [onUnload]);
+
+  const {
+    article,
+    currentUser,
+    comments,
+    commentErrors,
+    match,
+  } = props;
+
+  if (!article) {
+    return null;
   }
 
-  componentWillUnmount() {
-    this.props.onUnload();
-  }
+  const canModify = currentUser &&
+    currentUser.username === article.author.username;
+  return (
+    <div className="article-page">
 
-  render() {
-    const {
-      article,
-      currentUser,
-      comments,
-      commentErrors,
-      match,
-    } = this.props;
+      <div className="banner">
+        <div className="container">
 
-    if (!article) {
-      return null;
-    }
+          <h1>{article.title}</h1>
+          <ArticleMeta
+            article={article}
+            articleActions={(
+              <ArticleActions canModify={canModify} article={article} />
+            )}
+          />
 
-    const canModify = currentUser &&
-      currentUser.username === article.author.username;
-    return (
-      <div className="article-page">
-
-        <div className="banner">
-          <div className="container">
-
-            <h1>{article.title}</h1>
-            <ArticleMeta
-              article={article}
-              articleActions={(
-                <ArticleActions canModify={canModify} article={article} />
-              )}
-            />
-
-          </div>
-        </div>
-
-        <div className="container page">
-
-          <div className="row article-content">
-            <div className="col-xs-12">
-
-              <ArticleBody body={article.body} />
-
-              <ul className="tag-list">
-                {
-                  article.tagList.map(tag => {
-                    return (
-                      <li
-                        className="tag-default tag-pill tag-outline"
-                        key={tag}>
-                        {tag}
-                      </li>
-                    );
-                  })
-                }
-              </ul>
-
-            </div>
-          </div>
-
-          <hr />
-
-          <div className="article-actions">
-          </div>
-
-          <div className="row">
-            <CommentContainer
-              comments={comments || []}
-              errors={commentErrors}
-              slug={match.params.id}
-              currentUser={currentUser}
-            />
-          </div>
         </div>
       </div>
-    );
-  }
+
+      <div className="container page">
+
+        <div className="row article-content">
+          <div className="col-xs-12">
+
+            <ArticleBody body={article.body} />
+
+            <ul className="tag-list">
+              {
+                article.tagList.map(tag => {
+                  return (
+                    <li
+                      className="tag-default tag-pill tag-outline"
+                      key={tag}>
+                      {tag}
+                    </li>
+                  );
+                })
+              }
+            </ul>
+
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="article-actions">
+        </div>
+
+        <div className="row">
+          <CommentContainer
+            comments={comments || []}
+            errors={commentErrors}
+            slug={match.params.id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Article;
