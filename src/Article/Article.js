0f254953abc2c8653646796a4d5dd72f9d03ed22
Piotr Niewiejski <piotrniewiejski@gmail.com>
2019-12-22 08:18:17 +0100

    Exercise 17 - Refactor `Article` component to Hooks

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ArticleMeta from '../components/ArticleMeta';
 import ArticleBody from './ArticleBody';
 import CommentContainer from './CommentContainer';
 import ArticleActions from './ArticleActions';
 
-class Article extends React.Component {
-  componentWillMount() {
-    const articleId = this.props.match.params.id;
-    this.props.fetchArticle(articleId);
+function Article(props) {
+  const { 
+    article,
+    currentUser,
+    comments,
+    commentErrors,
+    match: { params: { id: articleId } },
+    fetchArticle,
+    onUnload
+  } = props;
+
+  useEffect(() => {
+    fetchArticle(articleId);
+
+    return () => onUnload();
+  }, [articleId, fetchArticle, onUnload]);
+
+  if (!article) {
+    return null;
   }
 
-  componentWillUnmount() {
-    this.props.onUnload();
-  }
-
-  render() {
-    const { 
-      article,
-      currentUser,
-      comments,
-      commentErrors,
-      match
-    } = this.props;
-    if (!article) {
-      return null;
-    }
-
-    const canModify = currentUser &&
-      currentUser.username === article.author.username;
-    return (
-      <div className="article-page">
+  const canModify = currentUser &&
+    currentUser.username === article.author.username;
+  return (
+    <div className="article-page">
 
       <div className="banner">
         <div className="container">
@@ -44,40 +43,39 @@ class Article extends React.Component {
         </div>
       </div>
 
-        <div className="container page">
-          <div className="row article-content">
-            <div className="col-xs-12">
-              <ArticleBody body={article.body}/>
-              <ul className="tag-list">
-                {
-                  this.props.article.tagList.map(tag => {
-                    return (
-                      <li
-                        className="tag-default tag-pill tag-outline"
-                        key={tag}>
-                        {tag}
-                      </li>
-                    );
-                  })
-                }
-              </ul>
-            </div>
-          </div>
-          <hr />
-          <div className="article-actions">
-          </div>
-
-          <div className="row">
-            <CommentContainer
-              comments={comments || []}
-              errors={commentErrors}
-              slug={match.params.id}
-              currentUser={currentUser} />
+      <div className="container page">
+        <div className="row article-content">
+          <div className="col-xs-12">
+            <ArticleBody body={article.body}/>
+            <ul className="tag-list">
+              {
+                article.tagList.map(tag => {
+                  return (
+                    <li
+                      className="tag-default tag-pill tag-outline"
+                      key={tag}>
+                      {tag}
+                    </li>
+                  );
+                })
+              }
+            </ul>
           </div>
         </div>
+        <hr />
+        <div className="article-actions">
+        </div>
+
+        <div className="row">
+          <CommentContainer
+            comments={comments || []}
+            errors={commentErrors}
+            slug={articleId}
+            currentUser={currentUser} />
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Article;
